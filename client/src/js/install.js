d6c7eb323797a8ae2c431ed01fff31c935939c67
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-26 21:35:22 +0100

    butinstall event listener

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,7 +9,19 @@ window.addEventListener('beforeinstallprompt', (event) => {// Stores the trigger
     butInstall.classList.toggle("hidden", false);});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => { const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+      return;
+    }
+  
+    // Shows prompt
+    promptEvent.prompt();
+  
+    // Resets the deferred prompt variable, it can only be used once
+    window.deferredPrompt = null;
+  
+    butInstall.classList.toggle("hidden", true);});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});

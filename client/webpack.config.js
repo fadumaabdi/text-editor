b30885444065ce96185a18dc52fddfbab691ff51
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-26 21:33:17 +0100

    css loaders babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -54,7 +54,25 @@ module.exports = () => {
  //css loaders babel to webpack
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel-loader -> in order to use ES6
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
